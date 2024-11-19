697862e886c9ec8d2bfc7faecc2486066a387641
MHibriziF <hibrizifarghana@gmail.com>
2024-11-19 09:05:47 +0700

    connect with pbp_django_auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:easyshop/screens/menu.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,15 +13,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'EasyShop',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.deepOrange,
-        ).copyWith(secondary: Colors.deepPurple[100]),
-        useMaterial3: true,
+    return Provider(
+        create: (_) {
+          CookieRequest request = CookieRequest();
+          return request;
+        }, child: MaterialApp(
+        title: 'EasyShop',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.deepOrange,
+          ).copyWith(secondary: Colors.deepPurple[100]),
+          useMaterial3: true,
+        ),
+        home: MyHomePage(),
       ),
-      home: MyHomePage(),
     );
   }
 }
